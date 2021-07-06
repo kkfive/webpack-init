93cee09b1fa81a29b00bb4f64133d768fbd1e564
DreamyTZK <dreamytzk@outlook.com>
2021-07-06 22:11:42 +0800

    ✨ feat: 添加字体示例

--- webpack/loader/font.js
+++ webpack/loader/font.js
@@ -6,16 +6,20 @@
  * @LastEditTime: 2021-06-22 22:49:12
  * @LastEditors: 小康
  */
-
+const dev = {
+  esModule: false,
+  name: 'assets/font/[name]_[hash:8].[ext]'
+}
+const pro = {
+  esModule: false,
+  name: '[name]_[hash:8].[ext]',
+  outputPath: '/assets/font/',
+  publicPath: '../font/'
+}
 const font = {
   test: /\.(woff|woff2|eot|ttf|otf)$/i,
   loader: 'file-loader',
-  options: {
-    esModule: false,
-    name: '[name]_[hash:8].[ext]',
-    outputPath:'/assets/font/',
-    publicPath:'../font/'
-  }
+  options: process.env.NODE_ENV === 'production' ? pro : dev
 }
 
 module.exports = font
